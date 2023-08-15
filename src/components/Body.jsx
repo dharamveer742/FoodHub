7792c5718646e12c0a4b3c359b7f2515855313af
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-08-15 22:45:06 +0530

    added link on restaurants to redirect reasataurant menu

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,67 +2,86 @@ import { useState } from "react";
 import "./body.scss";
 import { swiggy_api_URL } from "../constants";
 import Shimmer from "./Shimmer";
-import { filterData } from "../utils/helper";
+import filterData  from "../utils/helper";
 import RestaurantCard from "./RestaurantCard";
 import { Link } from "react-router-dom";
 import useResData from "../hooks/useRestaurantData"
 
 
-const Body = () => {
-  const [searchText, setSearchText] = useState(""); // state variable for input value
-  const [allRestaurants] = useResData(swiggy_api_URL); // hook calling swiggy api to get restaurants
-  const [searchedData, setSearchedData] = useState(null); // state variable for search results
-  const [errorMessage, setErrorMessage] = useState(null);
 
+
+const Body = () => {
+ // const [searchText, setSearchText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [allRestaurants] = useResData(swiggy_api_URL);
+  const [filteredRestaurants, setFilteredRestaurants] = useState(null);  //
+  
+ 
+  // if user is not Online then return UserOffline component
+  
+
+  // use searchData function and set condition if data is empty show error message
   const searchData = (searchText, restaurants) => {
-    // trim whitspaces before searching
     if (searchText !== "") {
-      const data = filterData(searchText, restaurants);
-      setSearchedData(data);
+      const filteredData = filterData(searchText, restaurants);
+      setFilteredRestaurants(filteredData);
       setErrorMessage("");
-      if (data.length === 0) {
+      if (filteredData?.length === 0) {
         setErrorMessage(
           `Sorry, we couldn't find any results for "${searchText}"`
         );
       }
-    } else {
+    } 
+    else {
       setErrorMessage("");
-      setSearchedData(restaurants);
+      setFilteredRestaurants(restaurants);
     }
   };
 
-
-
   
+  if (!allRestaurants) return null;
+
   return (
     <>
       <div className="search-container">
         <input
-          placeholder="search a restaurant"
           type="text"
           className="search-input"
+          placeholder="Search a restaurant you want..."
+          
+          
           onChange={(e) => {
-            setSearchText(e.target.value);
+            // when user will enter the data, it automatically called searchData function so it work same as when you click on Search button
             searchData(e.target.value, allRestaurants);
           }}
         ></input>
-        <button className="search-btn">Search</button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // user click on button searchData function is called
+            searchData(searchText, allRestaurants);
+          }}
+        >
+          Search
+        </button>
       </div>
-
       {errorMessage && <div className="error-container">{errorMessage}</div>}
 
-      {allRestaurants?.length == 0 ? (
-        <Shimmer></Shimmer>
+      {/* if restaurants data are fetched then display restaurants cards otherwise display Shimmer UI */ }
+      {allRestaurants?.length === 0 ? (
+        <Shimmer />
       ) : (
         <div className="restaurant-list">
-          {(searchedData === null ? allRestaurants: searchedData).map(
+          {/* We are mapping restaurants array and passing JSON array data to RestaurantCard component as props with unique key as restaurant.data.id */}
+          {(filteredRestaurants === null ? allRestaurants : filteredRestaurants).map(
             (restaurant) => {
               return (
                 <Link
-                  to={"/restaurant/" + restaurant.data.id}
-                  key={restaurant.data.id}
+                  to={"/restaurant/" + restaurant?.info?.id}
+                  key={restaurant?.info?.id}
                 >
-                  <RestaurantCard {...restaurant.data} />
+                  {/* if we click on any restaurant card it will redirect to that restaurant menu page */}
+                  <RestaurantCard {...restaurant?.info} />
                 </Link>
               );
             }
@@ -72,5 +91,15 @@ const Body = () => {
     </>
   );
 };
-
 export default Body;
+/*
+data fetch 
+restaurant cards
+search
+
+to fix
+header sticky
+routes
+restaurant card labels
+
+*/
