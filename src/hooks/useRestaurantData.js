db50093cb75a0cfc5ccf9484a7d505d5d80c3e43
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-08-15 22:39:50 +0530

    create a custom hook to fetch resataurant info and menu items

--- src/hooks/useRestaurantData.js
+++ src/hooks/useRestaurantData.js
@@ -2,33 +2,46 @@ import { useEffect, useState } from "react";
 
 const useResData = (API_URL) => {
   const [allRestaurants, setAllRestaurants] = useState([]);
-  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  
 
-  // use useEffect for one time call getRestaurants using empty dependency array
+  
   useEffect(() => {
     getRestaurants();
   }, []);
 
-  // async function getRestaurant to fetch API data
+  
   async function getRestaurants() {
-    // handle the error using try... catch
+    
     try {
       const response = await fetch(API_URL);
-        // if response is not ok then throw new Error
+      
       if (!response.ok) {
         const err = response.status;
         throw new Error(err);
-      } else {
+      } 
+      else {
         const json = await response.json();
-        // updated state variable restaurants with Swiggy API data
-        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+ 
+        async function checkJsonData(jsonData) {
+          for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+            let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            if (checkData !== undefined) {
+              return checkData;
+            }
+          }
+        }
+
+        const resData = await checkJsonData(json);
+        setAllRestaurants(resData);
+        
       }
-    } catch (error) {
-      console.error(error); // show error in console
+    } catch (err) {
+      console.error(err); 
     }
   }
-  return [allRestaurants, filteredRestaurants];
+  
+  return [allRestaurants]; 
 };
 
 export default useResData;
+
