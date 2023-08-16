b4fa60b92f5ace3936aa833762c9ad39c98ea755
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-08-16 20:37:52 +0530

    added Footer for pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import "./Footer.scss";
 const Footer= ()=>{
     return(
-        <h1>Footer</h1>
+        <div className="Footer">
+            <div className="social-media">
+            <i className="fa-brands fa-square-github"></i>
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-square-google-plus"></i>
+        </div>
+        <div className="footer-bottom">
+            <p id="copyright">Copyright @2023 Designed by DHARAMVEER</p>
+        </div>
+        
+    </div>
     )
 }
 export default Footer;
